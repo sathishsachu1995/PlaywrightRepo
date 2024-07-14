33144f165d86376143f06f146267cfe0eeb20238
sathishsachu1995 <154673827+sathishsachu1995@users.noreply.github.com>
2024-07-14 22:32:26 +0530

    Update LoginPage.ts

--- Pages/LoginPage.ts
+++ Pages/LoginPage.ts
@@ -1,37 +1,24 @@
 // creating LoginPage class for to login to the prelive WOA customer portal
 
 import { Page,BrowserContext, expect  } from "@playwright/test";  // importing page and browser context fixture from playwright/test module
+import { PlaywrightWrapper } from "../Utils/playwright";  // importing PlaywrightWrapper class from the Utils folder
+import { UrlConstants } from "../Constants/urlConstants"; // importing Enum name -->UrlConstants from the Constants folder
 
-export class LoginPage{  // creating class called login page
-    private page: Page  // creating page property as private and return type is Page
-    private context: BrowserContext  // creating context property as private and return type is BrowserContext
+export class LoginPage extends PlaywrightWrapper{  // creating class called login page which is extending base class called PlaywrightWrapper to use those methods
 
-    constructor(page:Page,context:BrowserContext){  /* creating constructor for LoginPage and passing page and context property as a argument. 
-                                                       when we call this constructor while creating object for this LoginPage class, page and context will be passed inside constructor
-                                                       from there this constructor will initialize values to the page and context property of this current LoginPage class */
+    static preliveLoginUrl = UrlConstants.loginPageUrl // calling Enum string loginPageUrl and store it in a static variable so that we can call this loginPageUrl with class name LoginPage
 
-        this.page = page  /* this keyword refers to current class and current class page property = page property which is passed as an argument inside constructor.
-                            Here initialization is done for the current class page property */
+    constructor(page:Page,context:BrowserContext){ // Creating constructor for the curren class and passing page and context as a argument.. while creating object for this LoginPage class page and context value will be initialized
+        super(page,context)  /* Calling parent class constructor using Super() keyword to ovveride its properties while creating object for LoginPage we have to pass Page and Context argument value and 
+                                it will go to parent class constructor and page,context will be initialized and those page and context will be passed to this current LoginPage class */
 
-        this.context = context  /* Current class context property = context property which is passed as an argument inside constructor.
-                                   Here initialization is done for the current class context property */
     }
 
     // For this method value's will be implementing from JSON file
-    async preliveLogin(url: string,username: string, password: string): Promise<void>{  // Creating preliveLogin aysnc function and passing url ,username and password as an argument
-        await this.page.goto(url)  // Using current class page to use page method -> goto(url) and it will return promise so we should use await keyword to resolve promise
-        
-        const loginID = this.page.getByPlaceholder("Enter username") // using getByPlaceholder method for to find username element
-        await loginID.fill(username)  // Using fill method to fill value to the username field
-        await expect(loginID,`Username text field is available to fill username ${username}`).toBeEnabled() // using Assertion to check whether username element is enabled or editable
-
-        const loginPassword = this.page.getByPlaceholder("Enter password")  // using getByPlaceholder method for to find password element
-        await loginPassword.fill(password)  // Using fill method to fill value to the password field
-        await expect(loginPassword,`Password text field is available to fill password ${password}`).toBeEnabled() // using Assertion to check whether password element is enabled or editable
-
-        const loginButton = this.page.getByText("Continue")  // using getByText method for to find continue button
-        await loginButton.click()  // Using click method to click the button
-        await expect(loginButton,`Continue button is available to click the Continue button`).toBeEnabled() // using Assertion to check whether click button is enabled or clickable
-
+    async preliveLogin(username: string, password: string): Promise<void>{  // Creating preliveLogin aysnc function and passing username and password as an argument
+       await this.loadAppUrl(LoginPage.preliveLoginUrl)  // Calling loadAppUrl method from the WrapperClass PlaywrightWrapper to load the prelive login Url we have to use 'this' keyword while calling methods from wrapper class this means current class
+       await this.type("[placeholder='Enter username']","Username",username)  // Calling type method from the WrapperClass PlaywrightWrapper to enter username in the username field. Inside method we have to pass 'locator', 'name' of the element and 'value'
+       await this.type("[placeholder='Enter password']","Password",password)  // Calling type method from the WrapperClass PlaywrightWrapper to enter password in the password field. Inside method we have to pass 'locator', 'name' of the element and 'value'
+       await this.clickButton('text= Continue',"Continue","Button")  // Calling clickButton method from the WrapperClass PlaywrightWrapper to click the login button. Inside method we have to pass 'locator', 'name' of the button and 'type' of the button
     }
 }

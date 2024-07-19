23269b599953f03035c10e87ee4734f9aa024e6d
sathishsachu1995 <154673827+sathishsachu1995@users.noreply.github.com>
2024-07-19 17:00:43 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 2,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -29,9 +29,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: false,
-    video: 'on-first-retry',
+    video: 'on',
     screenshot: 'only-on-failure',
-    actionTimeout : 3000,
+    actionTimeout : 8000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
@@ -44,7 +44,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
-      viewport:{ width: 1500, height: 1080 },
+      viewport:{ width: 1900, height: 1080 },
       },
     },
 

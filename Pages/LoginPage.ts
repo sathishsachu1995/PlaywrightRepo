3c3450b4e2c1831c22cc69a5f1253b96152d74d0
sathishsachu1995 <154673827+sathishsachu1995@users.noreply.github.com>
2024-07-19 16:54:59 +0530

    Update LoginPage.ts

--- Pages/LoginPage.ts
+++ Pages/LoginPage.ts
@@ -15,10 +15,11 @@ export class LoginPage extends PlaywrightWrapper{  // creating class called logi
     }
 
     // For this method value's will be implementing from JSON file
-    async preliveLogin(username: string, password: string): Promise<void>{  // Creating preliveLogin aysnc function and passing username and password as an argument
+    async preliveLogin(loginID: string, pswrd: string): Promise<void>{  // Creating preliveLogin aysnc function and passing username and password as an argument
        await this.loadAppUrl(LoginPage.preliveLoginUrl)  // Calling loadAppUrl method from the WrapperClass PlaywrightWrapper to load the prelive login Url we have to use 'this' keyword while calling methods from wrapper class this means current class
-       await this.type("[placeholder='Enter username']","Username",username)  // Calling type method from the WrapperClass PlaywrightWrapper to enter username in the username field. Inside method we have to pass 'locator', 'name' of the element and 'value'
-       await this.type("[placeholder='Enter password']","Password",password)  // Calling type method from the WrapperClass PlaywrightWrapper to enter password in the password field. Inside method we have to pass 'locator', 'name' of the element and 'value'
+       await this.type("[placeholder='Enter username']","Username",loginID)  // Calling type method from the WrapperClass PlaywrightWrapper to enter username in the username field. Inside method we have to pass 'locator', 'name' of the element and 'value'
+       await this.type("[placeholder='Enter password']","Password",pswrd)  // Calling type method from the WrapperClass PlaywrightWrapper to enter password in the password field. Inside method we have to pass 'locator', 'name' of the element and 'value'
        await this.clickButton('text= Continue',"Continue","Button")  // Calling clickButton method from the WrapperClass PlaywrightWrapper to click the login button. Inside method we have to pass 'locator', 'name' of the button and 'type' of the button
+       await this.spin("//div[@class='la-ball-beat la-2x']")
     }
 }
